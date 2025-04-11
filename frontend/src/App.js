fd8815fd19d6a48f748c5e95054b9650f76ddc4f
JunyIIIChen <cjy799255537@gmail.com>
2025-04-11 23:43:49 +1000

    develop create homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Navbar } from './Component/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import LoginSignup from './Pages/LoginSignup';
-import { Shop } from './Pages/Shop';
+import { LandingPage } from './Pages/LandingPage.jsx';
 import { Footer } from './Component/Footer/Footer.jsx'
 
 
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Navbar></Navbar>
         <Routes>
-          <Route path='/' element={<Shop></Shop>} />
+          <Route path='/' element={<LandingPage></LandingPage>} />
           <Route path='login' element={<LoginSignup></LoginSignup>}></Route>
         </Routes>
         <Footer/>
--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Hero } from '../Component/Hero/Hero'
 
 
-export const Shop = () => {
+export const LandingPage = () => {
   return (
       <div>
       <Hero />
