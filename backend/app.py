dd9ef06293eb4f1ac246ad34c69628a906c7f205
lwuu0005 <lwuu0005@student.monash.edu>
2025-04-30 18:44:34 +1000

    environment prepare

--- backend/app.py
+++ backend/app.py
@@ -637,5 +637,5 @@ Return only a valid JSON array like:
 
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
-    # app.run(host='0.0.0.0', port=5001)
+    # app.run(port=5001,debug=True)
+    app.run(host='0.0.0.0', port=5001)
